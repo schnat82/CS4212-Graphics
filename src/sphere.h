22e6e36d71825cfb2f2b2cc603ce08aa587d6b34
schnat82 <146293050+schnat82@users.noreply.github.com>
2026-09-23 13:56:11 -0500

    Add src/sphere.h for sphere intersection lab

---
+++ src/sphere.h
@@ -0,0 +1,21 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "shape.h"
+#include "vec3.h"
+
+class Sphere : public Shape {
+public:
+    Sphere(const point& center, double radius);
+
+    bool intersect(const ray& r) const override;
+
+    const point& center() const;
+    double radius() const;
+
+private:
+    point sphereCenter;
+    double sphereRadius;
+};
+
+#endif
